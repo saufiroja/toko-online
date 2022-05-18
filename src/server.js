3186beb23dd7715860baf9e2b2d10879187ed04f
saufiroja <saufiroja08@gmail.com>
2022-05-18 18:20:59 +0800

    chore:

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`connect on port ${PORT}`);
+});
