6d508f0630c0f7c5568d1163cb465b1e45438f1f
saufiroja <saufiroja08@gmail.com>
2022-05-28 19:10:35 +0800

    docs: add payload jwt

--- src/lib/generateToken.js
+++ src/lib/generateToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET, JWT_EXPIRESIN } = process.env;
 
 const generateAccessToken = (user) => {
-  const payload = { id: user.id };
+  const payload = { id: user.id, role: user.role };
   const result = jwt.sign(payload, JWT_SECRET, {
     expiresIn: JWT_EXPIRESIN,
   });
