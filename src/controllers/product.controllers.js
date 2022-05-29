b285d98224f55358bc76d9b98f12ef9051f008c5
saufiroja <saufiroja08@gmail.com>
2022-05-29 15:52:43 +0800

    feat: add route get, delete and put

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -26,4 +26,106 @@ const createProduct = async (req, res, next) => {
   }
 };
 
-module.exports = { createProduct };
+const getAllProducts = async (req, res, next) => {
+  try {
+    const products = await Product.findAndCountAll({
+      limit: 10,
+      offset: 0,
+    });
+    if (!products) {
+      return res.status(404).json({
+        message: 'product not found',
+      });
+    }
+
+    return res.status(200).json({
+      message: 'success get all products',
+      products,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+const getProductById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
+    if (!product) {
+      return res.status(404).json({
+        message: 'product not found',
+      });
+    }
+
+    return res.status(200).json({
+      message: 'success get product by id',
+      product,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateProduct = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const { id } = req.params;
+    const { product, price } = req.body;
+
+    if (user.role == 'User') {
+      return res.status(400).json({
+        message: 'unautorized',
+      });
+    }
+
+    const products = await Product.update(
+      { product, price },
+      { where: { id } }
+    );
+
+    if (!products) {
+      return res.status(404).json({
+        message: 'product not found',
+      });
+    }
+
+    return res.status(201).json({
+      message: 'success update product',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteProduct = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const { id } = req.params;
+
+    if (user.role == 'User') {
+      return res.status(400).json({
+        message: 'unauthorization',
+      });
+    }
+
+    const product = await Product.destroy({ where: { id } });
+    if (!product) {
+      return res.status(404).json({
+        message: 'product not found',
+      });
+    }
+
+    return res.status(200).json({
+      message: 'success delete product',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+};
--- src/routers/product.routers.js
+++ src/routers/product.routers.js
@@ -1,9 +1,22 @@
 const express = require('express');
-const { createProduct } = require('../controllers/product.controllers');
+const {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} = require('../controllers/product.controllers');
 const { authorization } = require('../middlewares/auth.middlewares');
 
 const router = express.Router();
 
+router.get('/products', getAllProducts);
+router.get('/product/:id', getProductById);
+
 router.post('/products', authorization, createProduct);
+
+router.put('/product/:id', authorization, updateProduct);
+
+router.delete('/product/:id', authorization, deleteProduct);
 
 module.exports = router;
